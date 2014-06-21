8ad122d03dee15f46746f92a49faf8e8bed49ba1
NylixKazag <benmomo1@gmail.com>
2014-06-21 12:09:22 -0700

    Cleaned up Caeser's Cipher class.

--- Cryptology/src/cryptology/ciphers/Caesar.java
+++ Cryptology/src/cryptology/ciphers/Caesar.java
@@ -12,6 +12,10 @@ public class Caesar extends Cryptology {
 
 	public String encipher(String clearText, int shiftValue) {
 		
+		/*
+		 *  New Algorithm - Wrap values outside of character set back around instead
+		 */
+		
 		String finCipherText = "";
 
 		String finClearText = stripNonLetter(clearText);
@@ -20,21 +24,14 @@ public class Caesar extends Cryptology {
 		for (int i = 0; i < finClearText.length(); i++) {
 			int character = (finClearText.charAt(i) + shiftValue);
 			
-			// Old Algorithm - DOES NOT SUPPORT SHIFT VALUES > 26 OR < 0
-			/*
-			if (character > 90) {
-				//character = (char)(character - 26);
-				character -= 26;
-			}
-			*/
-			
-			// New Algorithm - Wrap values outside of character set back around
-			character -= 64;		// Shift down from ascii values
-			character %= 26;		// Get remainder after dividing 26
-			if (character <= 0)		// (Special case if shift value is negative) OR (remainder is 0 so it must be z)
+			character -= 64;			// Shift down from ascii values (Shift from 65-90 to 1-26)
+			character %= 26;			// Get remainder after dividing 26 to ensure that the value is within 1-26
+			if (character <= 0) {		// (Special case if shift value is negative) OR (remainder is 0 so it must be z)
+				
 				character += 26;
-			character += 64;		// Shift back up to ascii values
-			finCipherText += String.valueOf((char)character); // Append to final string
+			}
+			character += 64;			// Shift back up to ascii values (Shift from 1-26 to)
+			finClearText += String.valueOf((char)character);		// Append to final string
 		}
 		return finCipherText;
 	}
