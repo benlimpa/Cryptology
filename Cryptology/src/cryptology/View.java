ad11737741c066cf47b3f18ad8bad804dc8f87d3
JavierKazag <javier@javierandre.com>
2014-06-28 10:45:09 -0700

    Added getText() to View class. Accesss Cryptology methods statically.

--- Cryptology/src/cryptology/View.java
+++ Cryptology/src/cryptology/View.java
@@ -1,5 +1,53 @@
 package cryptology;
 
+import java.util.Scanner;
+
 public class View {
 
+	private Scanner	keyboard;
+	
+	public View() {
+		
+	}
+	
+	/*
+	 * Get Text Method
+	 * 
+	 */
+	
+	private String getText(String type) {
+		
+		String 	text 		= "";
+		String  input 		= "";
+		boolean done 		= false;
+		
+		System.out.println("\n" + "Enter each line of the " + type + " below and press Enter. Press Enter again when you are finished:" + "\n");
+		
+		while (!done) {
+			
+			System.out.print("> ");
+			input = keyboard.nextLine();
+			input = Cryptology.stripNonLetter(input);
+			
+			if (input.length() > 0) {
+				
+				text = text + input;
+			}
+			else {
+				
+				if (text.length() > 0) {
+					
+					System.out.println("\n" + "Text input finished.");
+				}
+				else {
+					
+					System.out.println("\n" + "Text input cancelled.");
+				}
+				
+				done = true;
+			}
+		}
+		text = text.toUpperCase();
+		return text;
+	}
 }
