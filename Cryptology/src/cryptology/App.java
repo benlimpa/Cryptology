10c03ab7d37d9e9bdd981c7e9c8d640e7f31768a
nylatiki <benmomo1@gmail.com>
2014-07-26 11:22:39 -0700

    Changed app to use javafx

--- Cryptology/src/cryptology/App.java
+++ Cryptology/src/cryptology/App.java
@@ -1,13 +1,21 @@
 package cryptology;
 
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
 
 	public static void main(String[] args) {
 		
-		View view = new View();
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		Controller controller = new Controller();
 		
-		view.displayMenu();
-		
+		View view  = new View(primaryStage, controller);
 	}
 
 }
