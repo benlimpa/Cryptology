79f163b371d08b47935a45614463de328594c00c
JavierKazag <javier@javierandre.com>
2014-07-26 11:21:18 -0700

    Added ColumnarView class, CSS folder and cryptology.css file.

---
+++ Cryptology/src/cryptology/ColumnarView.java
@@ -0,0 +1,42 @@
+package cryptology;
+
+import java.awt.Dimension;
+
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class ColumnarView extends VBox {
+
+	/*
+	 * Class Instance Variables
+	 */
+	
+	
+	
+	/*
+	 * Class Constants
+	 */
+	
+	
+	
+	/*
+	 * Constructor Method
+	 */
+	
+	public ColumnarView(Dimension columnarViewDimension) {
+	
+		TabPane tabPane = new TabPane();
+		tabPane.setStyle("-fx-font-size: 12pt;");
+		getChildren().add(tabPane);
+		
+		Tab singleTab = new Tab();	
+		singleTab.setText("Single");
+		singleTab.setContent(new Rectangle(800,400, Color.LIGHTBLUE));
+		singleTab.setClosable(false);
+		tabPane.getTabs().add(singleTab);
+		
+	}
+}
